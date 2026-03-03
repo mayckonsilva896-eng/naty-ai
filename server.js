3e75f5974bd80f021f8234b38c24ade8f3b30b72
mayckonsilva896-eng <mayckonsilva896@gmail.com>
2026-03-03 02:23:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static("public"));
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
