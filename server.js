7ee635a9a02cd1707d892751afd35baa08203a01
mayckonsilva896-eng <mayckonsilva896@gmail.com>
2026-03-03 02:28:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.post("/chat", async (req, res) => {
   res.json({ reply: response.choices[0].message.content });
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
 });
